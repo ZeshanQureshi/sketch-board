f2af206f3527c78478bb86516389a8af8a4ddde8
Zeshan Qureshi <zeshan@ubuntu.com>
2022-01-09 20:38:17 -0500

    Updated Grid Layout
    Added grid layout to main.js and implemented style to style.css

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,10 @@
-let container = document.getElementById("grid");
+const container = document.getElementById("grid");
 
 function initializeGrid(numPixels) {
 
     for (i = 0 ; i < numPixels ; i++) {
         for (j = 0 ; j < numPixels ; j++) {
-            grid.appendChild(document.createElement("div"));
+            grid.appendChild(document.createElement("div")).classList.add("layout");
         }
 
     }
