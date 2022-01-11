1a325e710cb4cfd71349e69c9d917691bc18e45b
Zeshan Qureshi <zeshan@ubuntu.com>
2022-01-10 22:10:02 -0500

    Updated drawing
    Completed drawing ability.

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,26 @@
-const container = document.getElementById("grid");
+var colourPicker = document.getElementById("colourSelect");
+colourPick = colourPicker.value;
 
-let colourPick = document.getElementById("colourSelect").value;
+colourPicker.addEventListener("input", function() {
+    colourPick = colourPicker.value;
+});
 
 function initializeGrid(numPixels) {
-
-    for (i = 0 ; i < numPixels ; i++) {
-        for (j = 0 ; j < numPixels ; j++) {
-            grid.appendChild(document.createElement("div")).classList.add("layout");
-        }
-
+    for (i = 0 ; i < numPixels ** 2; i++) {
+            var square = document.createElement("div");
+            square.style.backgroundColor = "white";
+            square.style.width = 720/numPixels + "px";
+            square.style.height = 720/numPixels + "px";
+            square.style.float = "left";
+            square.addEventListener("mouseover", function() {
+                this.style.backgroundColor = colourPick;
+            });
+            document.getElementById("grid").appendChild(square);
     }
-    
+}
+
+function clearBoard(numPixels) {
+    for (j = 0; j < numPixels ** 2; j++) {
+        document.getElementById("grid").children[j].style.backgroundColor = "white";
+    }
 }
