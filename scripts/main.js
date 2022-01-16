ecea78d5ec98fe87a2f027603f4b1f5851b7a7f6
Zeshan Qureshi <zeshan@ubuntu.com>
2022-01-15 21:05:16 -0500

    Completed SketchBoard
    Added functions to deleteBoard, updateColour, and updateSize.

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,24 @@
 var colourPicker = document.getElementById("colourSelect");
+var slider = document.getElementById("sizeSelect");
+var clearButton = document.getElementById("clearSelect");
+
 colourPick = colourPicker.value;
+
+slider.step = "8";
 
 colourPicker.addEventListener("input", function() {
     colourPick = colourPicker.value;
 });
+
+slider.addEventListener("input", function() {
+    deleteBoard();
+    initializeGrid(this.value);
+})
+
+clearButton.addEventListener("click", function() {
+    sliderValue = slider.value;
+    clearBoard(sliderValue);
+})
 
 function initializeGrid(numPixels) {
     for (i = 0 ; i < numPixels ** 2; i++) {
@@ -23,4 +38,8 @@ function clearBoard(numPixels) {
     for (j = 0; j < numPixels ** 2; j++) {
         document.getElementById("grid").children[j].style.backgroundColor = "white";
     }
+}
+
+function deleteBoard() {
+    document.getElementById("grid").querySelectorAll('*').forEach(n => n.remove());
 }
